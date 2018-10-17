2be0b7214b2e876e231f4d3d7ebc8a6d09107bef
Alexandre Lamarre <alexandrelamarre52@gmail.com>
2018-10-17 15:06:22 -0500

    Ajout en tête

--- TP2/TP2/main.c
+++ TP2/TP2/main.c
@@ -1,3 +1,13 @@
+/*
+Titre: TP2
+Description: Ce programme implémente les divers algorithmes de tri de tableaux sur place:
+le tri par sélection, le tri par insertion et le tri à bulles.
 
-#define CRT_SECURE_NO_WARNINGS
-//lgsldhg
+Auteur: Alexande Lamarre et Francis Alonzo
+Date: 2018-10-17
+*/
+
+/***********COMMANDES PRÉPROCESSEUR*************/
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdlib.h>
+#include <stdio.h>
