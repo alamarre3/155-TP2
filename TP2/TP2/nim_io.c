71efbd66066c2a30d4d0c5b7940a4516e7fc198d
supertoto29 <44241055+supertoto29@users.noreply.github.com>
2018-10-19 10:05:35 -0400

    Nim_io ajout

---
+++ TP2/TP2/nim_io.c
@@ -0,0 +1,25 @@
+/*=====MODULE NIM_IO======*/
+
+#include "nim_io.h"
+
+// Fonction Lire_entier
+
+int lire_entier(int min, int max) {
+
+	// Déclaration des variables
+	int entier; // Valeur d'un entier saisi par l'usage
+
+
+	scanf("%d", &entier);
+	if (entier >= min && entier <= max) {
+		return entier;
+	}
+	else {
+		printf("Valeur saisie non valide, veuillez saisir une valeur dans l'intervalle %d et %d.\n", min, max);
+		return -1;
+	}
+
+}
+
+ 
+
