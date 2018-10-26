456738315bed8995a61e97a6cccee5ea71e1aea0
supertoto29 <44241055+supertoto29@users.noreply.github.com>
2018-10-26 09:04:53 -0400

    Save locale

--- TP2/TP2/main.c
+++ TP2/TP2/main.c
@@ -11,3 +11,20 @@ Date: 2018-10-17
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdlib.h>
 #include <stdio.h>
+#include "nim_io.h"
+
+void main(void) {
+
+	int max = 100;
+	int min = 5;
+	int entier=-1;
+	do {
+		printf("Saisir une valeur entre %d et %d : ", min, max);
+		entier = lire_entier(min, max);
+	} while (entier == -1);
+
+	printf("Entier = %d.\n", entier);
+	system("pause");
+}
+
+
